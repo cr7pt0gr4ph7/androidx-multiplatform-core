4bd99855a4236a45037644c518830d182b19fdfc
Lukas Waslowski <cr7pt0gr4ph7@googlemail.com>
2023-08-31 17:15:27 +0000

    Add android.app.Application stub implementation.

--- polyfill/polyfill-common/src/jvmMain/kotlin/android/app/Application.jvm.kt
+++ polyfill/polyfill-common/src/jvmMain/kotlin/android/app/Application.jvm.kt
@@ -13,6 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package android.app
+package android.app
 
- public actual class Application
+public actual class Application
