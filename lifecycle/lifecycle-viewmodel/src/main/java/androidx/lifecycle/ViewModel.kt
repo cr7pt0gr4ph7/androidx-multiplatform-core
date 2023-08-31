60d1838575a87b37e3f29b4642a99148ccb34db9
Lukas Waslowski <cr7pt0gr4ph7@googlemail.com>
2023-08-31 20:18:34 +0000

    Convert ViewModel code from Java to Kotlin.

--- lifecycle/lifecycle-viewmodel/src/main/java/androidx/lifecycle/ViewModel.kt
+++ lifecycle/lifecycle-viewmodel/src/main/java/androidx/lifecycle/ViewModel.kt
@@ -14,41 +14,34 @@
  * limitations under the License.
  */
 
-package androidx.lifecycle;
+package androidx.lifecycle
 
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import androidx.annotation.MainThread
+import java.io.Closeable
+import java.io.IOException
+import kotlin.concurrent.Volatile
 
 /**
  * ViewModel is a class that is responsible for preparing and managing the data for
- * an {@link android.app.Activity Activity} or a {@link androidx.fragment.app.Fragment Fragment}.
+ * an [android.app.Activity][Activity] or a [androidx.fragment.app.Fragment][Fragment].
  * It also handles the communication of the Activity / Fragment with the rest of the application
  * (e.g. calling the business logic classes).
- * <p>
+ *
  * A ViewModel is always created in association with a scope (a fragment or an activity) and will
  * be retained as long as the scope is alive. E.g. if it is an Activity, until it is
  * finished.
- * <p>
+ *
  * In other words, this means that a ViewModel will not be destroyed if its owner is destroyed for a
  * configuration change (e.g. rotation). The new owner instance just re-connects to the existing model.
- * <p>
+ *
  * The purpose of the ViewModel is to acquire and keep the information that is necessary for an
  * Activity or a Fragment. The Activity or the Fragment should be able to observe changes in the
  * ViewModel. ViewModels usually expose this information via {@link LiveData} or Android Data
  * Binding. You can also use any observability construct from your favorite framework.
- * <p>
- * ViewModel's only responsibility is to manage the data for the UI. It <b>should never</b> access
+ *
+ * ViewModel's only responsibility is to manage the data for the UI. It *should never* access
  * your view hierarchy or hold a reference back to the Activity or the Fragment.
- * <p>
+ *
  * Typical usage from an Activity standpoint would be:
  * <pre>
  * public class UserActivity extends Activity {
@@ -94,7 +87,6 @@ import java.util.Set;
  * }
  * </pre>
  *
- * <p>
  * ViewModels can also be used as a communication layer between different Fragments of an Activity.
  * Each Fragment can acquire the ViewModel using the same key via their Activity. This allows
  * communication between Fragments in a de-coupled fashion such that they never need to talk to
@@ -106,140 +98,135 @@ import java.util.Set;
  *     }
  * }
  * </pre>
- * </>
  */
 public abstract class ViewModel {
     // Can't use ConcurrentHashMap, because it can lose values on old apis (see b/37042460)
-    @Nullable
-    private final Map<String, Object> mBagOfTags = new HashMap<>();
-    @Nullable
-    private final Set<Closeable> mCloseables = new LinkedHashSet<>();
-    private volatile boolean mCleared = false;
+    private val mBagOfTags: MutableMap<String, Object>? = mutableMapOf()
+    private val mCloseables: MutableSet<Closeable>? = mutableSetOf()
+    @Volatile
+    private var mCleared: Boolean = false
 
     /**
-     * Construct a new ViewModel instance.
-     * <p>
-     * You should <strong>never</strong> manually construct a ViewModel outside of a
-     * {@link ViewModelProvider.Factory}.
-     */
-    public ViewModel() {
-    }
-
-    /**
-     * Construct a new ViewModel instance. Any {@link Closeable} objects provided here
-     * will be closed directly before {@link #onCleared()} is called.
-     * <p>
-     * You should <strong>never</strong> manually construct a ViewModel outside of a
-     * {@link ViewModelProvider.Factory}.
-     */
-    public ViewModel(@NonNull Closeable... closeables) {
-        mCloseables.addAll(Arrays.asList(closeables));
-    }
-
-    /**
-     * Add a new {@link Closeable} object that will be closed directly before
-     * {@link #onCleared()} is called.
+     * Construct a new [ViewModel] instance.
      *
-     * @param closeable The object that should be {@link Closeable#close() closed} directly before
-     *                  {@link #onCleared()} is called.
+     * You should *never* manually construct a [ViewModel] outside of a
+     * [ViewModelProvider.Factory].
      */
-    public void addCloseable(@NonNull Closeable closeable) {
+    constructor() {
+    }
+
+    /**
+     * Construct a new [ViewModel] instance. Any [Closeable] objects provided here
+     * will be closed directly before [onCleared] is called.
+     *
+     * You should *never* manually construct a [ViewModel] outside of a
+     * [ViewModelProvider.Factory].
+     */
+    constructor(vararg closeables: Closeable) {
+        mCloseables.addAll(closeables.asList())
+    }
+
+    /**
+     * Add a new [Closeable] object that will be closed directly before
+     * [onCleared] is called.
+     *
+     * @param closeable The object that should be [Closeable.close][closed] directly before
+     *                  [onCleared] is called.
+     */
+    internal fun addCloseable(closeable: Closeable) {
         // As this method is final, it will still be called on mock objects even
         // though mCloseables won't actually be created...we'll just not do anything
         // in that case.
         if (mCloseables != null) {
-            synchronized (mCloseables) {
-                mCloseables.add(closeable);
+            synchronized(mCloseables) {
+                mCloseables.add(closeable)
             }
         }
     }
 
     /**
-     * This method will be called when this ViewModel is no longer used and will be destroyed.
-     * <p>
+     * This method will be called when this [ViewModel] is no longer used and will be destroyed.
+     *
      * It is useful when ViewModel observes some data and you need to clear this subscription to
      * prevent a leak of this ViewModel.
      */
-    @SuppressWarnings("WeakerAccess")
-    protected void onCleared() {
+    protected open fun onCleared() {
     }
 
     @MainThread
-    final void clear() {
-        mCleared = true;
+    internal fun clear() {
+        mCleared = true
         // Since clear() is final, this method is still called on mock objects
         // and in those cases, mBagOfTags is null. It'll always be empty though
         // because setTagIfAbsent and getTag are not final so we can skip
         // clearing it
         if (mBagOfTags != null) {
-            synchronized (mBagOfTags) {
-                for (Object value : mBagOfTags.values()) {
+            synchronized(mBagOfTags) {
+                for (value in mBagOfTags.values) {
                     // see comment for the similar call in setTagIfAbsent
-                    closeWithRuntimeException(value);
+                    closeWithRuntimeException(value)
                 }
             }
         }
         // We need the same null check here
         if (mCloseables != null) {
-            synchronized (mCloseables) {
-                for (Closeable closeable : mCloseables) {
-                    closeWithRuntimeException(closeable);
+            synchronized(mCloseables) {
+                for (closeable in mCloseables) {
+                    closeWithRuntimeException(closeable)
                 }
             }
         }
-        onCleared();
+        onCleared()
     }
 
     /**
-     * Sets a tag associated with this viewmodel and a key.
-     * If the given {@code newValue} is {@link Closeable},
-     * it will be closed once {@link #clear()}.
-     * <p>
+     * Sets a tag associated with this [ViewModel] and a key.
+     * If the given [newValue] is [Closeable],
+     * it will be closed once [clear].
+     *
      * If a value was already set for the given key, this call does nothing and
      * returns currently associated value, the given {@code newValue} would be ignored
-     * <p>
-     * If the ViewModel was already cleared then close() would be called on the returned object if
-     * it implements {@link Closeable}. The same object may receive multiple close calls, so method
+     *
+     * If the ViewModel was already cleared then [Closeable.close] would be called on the returned object if
+     * it implements [Closeable]. The same object may receive multiple close calls, so method
      * should be idempotent.
      */
-    @SuppressWarnings("unchecked")
-    <T> T setTagIfAbsent(String key, T newValue) {
-        T previous;
-        synchronized (mBagOfTags) {
-            previous = (T) mBagOfTags.get(key);
+    internal open fun setTagIfAbsent<T>(key: String, newValue: T): T {
+        var previous: T
+        synchronized(mBagOfTags) {
+            previous = mBagOfTags.get(key) as T
             if (previous == null) {
-                mBagOfTags.put(key, newValue);
+                mBagOfTags.put(key, newValue)
             }
         }
-        T result = previous == null ? newValue : previous;
+        val result = previous ?: newValue
         if (mCleared) {
             // It is possible that we'll call close() multiple times on the same object, but
             // Closeable interface requires close method to be idempotent:
             // "if the stream is already closed then invoking this method has no effect." (c)
-            closeWithRuntimeException(result);
+            closeWithRuntimeException(result)
         }
-        return result;
+        return result
     }
 
     /**
      * Returns the tag associated with this viewmodel and the specified key.
      */
-    @SuppressWarnings({"TypeParameterUnusedInFormals", "unchecked"})
-    <T> T getTag(String key) {
+    internal open fun getTag<T>(key: String): T? {
         if (mBagOfTags == null) {
-            return null;
+            return null
         }
-        synchronized (mBagOfTags) {
-            return (T) mBagOfTags.get(key);
+        synchronized(mBagOfTags) {
+            return mBagOfTags.get(key) as T?
         }
     }
 
-    private static void closeWithRuntimeException(Object obj) {
-        if (obj instanceof Closeable) {
+    private fun closeWithRuntimeException(obj: Object) {
+        if (obj is Closeable) {
             try {
-                ((Closeable) obj).close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                (obj as Closeable).close()
+            } catch (e: IOException) {
+                throw RuntimeException(e)
             }
         }
     }
